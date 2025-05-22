fafa02f533247b6bd40bd6adf7a54e4eb248bc45
Isasa <Isasa_EGwork@outlook.com>
2025-05-22 19:56:53 +0900

    BitVectorの複合代入演算子の処理をbit長さが元の長さから変わらないように修正

--- BitVector/BitVector.h
+++ BitVector/BitVector.h
@@ -3,6 +3,8 @@
 #include <vector>
 #include <climits>
 #include <string>
+#include <tuple>
+#include <utility>
 
 template <typename unit=uint64_t>
 class BitVector {
@@ -24,7 +26,7 @@ public:
 	BitVector(const size_t size) 
 		: size_(size)
 	{
-		this->bitvec_.resize(int(size / unit_size_) + 1, 0);
+		this->bitvec_.resize(get_byteidx_from_seqidx(size - 1) + 1, 0);
 	}
 
 	BitVector(const std::string str_bit) 
@@ -34,47 +36,55 @@ public:
 	}
 
 	/*------------------------------- index変換 ------------------------------*/
+
+	// indexの種類
+	// seqidx: ビット列全体のidx．e.g 右から65番目であれば65となる
+	// byteidx, bitidx: 
+	//     byteidx * unit_size + bitidxとなる
+	//     つまり，byteidx が bitvec の idx で，bitidxはその位置の unit の何ビット目かを示す
+	//     e.g 右から65番目であれば，unit_size=68 とすると，byteidx=1, bitidx=4 となる
 private:
-	int conv_vecidx2bitidx(const int vecidx) { return vecidx * unit_size_; }
-	int conv_bitidx2vecidx(const int bitidx) { return int(bitidx / unit_size_); }
+	static int conv_vecidx2bitidx(const int vecidx) { return vecidx * unit_size_; }
+	static int conv_bitidx2vecidx(const int bitidx) { return int(bitidx / unit_size_); }
+
+	static int get_byteidx_from_seqidx(const int seqidx) { return int(seqidx / unit_size_); }
+	static int get_bitidx_from_seqidx(const int seqidx) { return int(seqidx % unit_size_); }
+
+	static std::pair<int, int> conv_seqidx2bytebitidx(const int seqidx) {
+		return std::make_pair<int, int>(get_byteidx_from_seqidx(seqidx), get_bitidx_from_seqidx(seqidx));
+	}
 
 	/*------------------------------ 演算子 -------------------------------*/
 public:
 	BitVector<unit>& operator&=(const BitVector<unit>& obv) {
-		// サイズ調整
-		int iter_count = std::min(this->bitvec_.size(), obv.bitvec_.size());
-		if (this->bitvec_.size() == iter_count) this->bitvec_.resize(obv.bitvec_.size());
+		// 左端の unit 以外を演算
+		for (auto i = 0; i < this->bitvec_.size() - 1; ++i) this->bitvec_[i] &= obv.bitvec_[i];
 
-		// 演算を実行
-		for (auto i = 0; i < iter_count; ++i) {
-			this->bitvec_[i] &= obv.bitvec_[i];
-		}
+		// 左端の unit を演算
+		int bitidx = get_bitidx_from_seqidx(size_ - 1);
+		this->bitvec_.back() &= (obv.bitvec_.back() & (std::numeric_limits<unit>::max() >> (unit_size_ - 1 - bitidx)));
 
 		return *this;
 	}
 
 	BitVector<unit>& operator|=(const BitVector<unit>& obv) {
-		// サイズ調整
-		int iter_count = std::min(this->bitvec_.size(), obv.bitvec_.size());
-		if (this->bitvec_.size() == iter_count) this->bitvec_.resize(obv.bitvec_.size());
+		// 左端の unit 以外を演算
+		for (auto i = 0; i < this->bitvec_.size() - 1; ++i) this->bitvec_[i] ^= obv.bitvec_[i];
 
-		// 演算を実行
-		for (auto i = 0; i < iter_count; ++i) {
-			this->bitvec_[i] |= obv.bitvec_[i];
-		}
+		// 左端の unit を演算
+		int bitidx = get_bitidx_from_seqidx(size_ - 1);
+		this->bitvec_.back() |= (obv.bitvec_.back() & (std::numeric_limits<unit>::max() >> (unit_size_ - 1 - bitidx)));
 
 		return *this;
 	}
 
 	BitVector<unit>& operator^=(const BitVector<unit>& obv) {
-		// サイズ調整
-		int iter_count = std::min(this->bitvec_.size(), obv.bitvec_.size());
-		if (this->bitvec_.size() == iter_count) this->bitvec_.resize(obv.bitvec_.size());
+		// 左端の unit 以外を演算
+		for (auto i = 0; i < this->bitvec_.size() - 1; ++i) this->bitvec_[i] &= obv.bitvec_[i];
 
-		// 演算を実行
-		for (auto i = 0; i < iter_count; ++i) {
-			this->bitvec_[i] ^= obv.bitvec_[i];
-		}
+		// 左端の unit を演算
+		int bitidx = get_bitidx_from_seqidx(size_ - 1);
+		this->bitvec_.back() ^= (obv.bitvec_.back() & (std::numeric_limits<unit>::max() >> (unit_size_ - 1 - bitidx)));
 
 		return *this;
 	}
@@ -118,8 +128,16 @@ public:
 
 	/*------------------------------ 操作 ------------------------------*/
 public:
-	void set(unit x) {
-		this->bitvec_[0] = x;
+	BitVector<unit>& set(const size_t pos, const bool val=1) {
+		auto [byteidx, bitidx] = conv_seqidx2bytebitidx(pos);
+
+		this->bitvec_[byteidx] |= unit(1 << bitidx);
+
+		return *this;
+	}
+
+	void reset() {
+		this->bitvec_.clear();
 	}
 
 
